decd9290c735802f5bb5c36748d9a201588e9498
cjbrunnen <cazgotsoul@gmail.com>
2016-08-23 18:03:17 +0100

    Game stopping

--- js/app.js
+++ js/app.js
@@ -1,66 +1,63 @@
 $(start);
 
-var score   = 0;
-var counter = 0;
+var score        = 0;
+var counter      = 0;
 var squaresArray = [];
-var playerScore = 0;
-var squares = $('.square');
-var objects = [
-{
-  name: "avocado",
-  img: "./css/avoForWeb.png",
-  speed: 1500,
-  score: 1
-}, {
-  name: "sombrero",
-  img: "http://vignette2.wikia.nocookie.net/clubpenguin/images/2/2f/Mini_Sombrero.png/revision/latest?cb=20140408163249",
-  speed: 1000,
-  score: 2
-}, {
-  name: "chilli",
-  img: "http://sweetclipart.com/multisite/sweetclipart/files/red_chili_pepper.png",
-  speed: 1500,
-  score: -2
-}, {
-  name: "tacoGif",
-  img: "http://66.media.tumblr.com/a6fb0c2ac0c5b74ef910083bea480406/tumblr_nefxiuSGpt1tha1vgo1_250.gif",
-  speed: 100,
-  score: 5
-}
+var playerScore  = 0;
+var squares      = $('.square');
+var timerInterval, squaresInterval;
+var objects      = [
+  {
+    name: "avocado",
+    img: "./css/avoForWeb.png",
+    speed: 1500,
+    score: 1
+  }, {
+    name: "sombrero",
+    img: "http://vignette2.wikia.nocookie.net/clubpenguin/images/2/2f/Mini_Sombrero.png/revision/latest?cb=20140408163249",
+    speed: 1000,
+    score: 2
+  }, {
+    name: "chilli",
+    img: "http://sweetclipart.com/multisite/sweetclipart/files/red_chili_pepper.png",
+    speed: 1500,
+    score: -2
+  }, {
+    name: "tacoGif",
+    img: "http://66.media.tumblr.com/a6fb0c2ac0c5b74ef910083bea480406/tumblr_nefxiuSGpt1tha1vgo1_250.gif",
+    speed: 100,
+    score: 5
+  }
 ]
 
-// function themePlay() {
-//   $('#theme').play();
-//   $('#theme').muted = false;
-// }
+function themePlay() {
+  $('#theme').play();
+  $('#theme').muted = false;
+}
 
-// function mute(){
-//   $('#mute').on("click", function (){
-//   if (theme.muted === false) {    
-//    theme.muted = true;
-//  } else {
-//   theme.muted = false;
-// }
-// }); 
-// };
+function mute(){
+  $('#mute').on('click', function (){
+    if (theme.muted === false) {    
+      theme.muted = true;
+    } else {
+      theme.muted  = false;
+    }
+  }); 
+}
 
 function start(){
   $('#go').on('click', go)
 }
 
 function go() {
-
-  var count       = 30;
-  var counterTwo  = setInterval(timer, 1000);
+  var count     = 5;
+  timerInterval = setInterval(timer, 1000);
 
   function timer() {
     count -= 1;  
-    if (count == 0){
-      endGame(counterTwo);
-    }
-    document.getElementById("timer").innerHTML = count;
+    if (count === 0) endGame();
+    $("#timer").html(count);
   }
-
 
   beginGame()
 }
@@ -68,7 +65,7 @@ function go() {
 function beginGame() {
   var squares = $('.square')
 
-  setInterval(function() {
+  squaresInterval = setInterval(function() {
     showSquare(squares);
   }, 1500);
 }
@@ -86,7 +83,7 @@ function showSquare(array) {
     $(square).css('background-image', 'none');
     $(square).off();
     counter++;
-    counterCount(counter, character)
+    // counterCount(counter, character)
   }, 1000)
 }
 
@@ -95,17 +92,16 @@ function logScore(character) {
   $(".scoreBoard").html(score);
 }
 
-// function counterCount(counter, character) {
-//   if (counter === 3) {
+// function counterCount(counter, character) {// if more than 3 misses APPART FROM CHILLIS
+//   if (counter === 3) {                     // then endGame
 //     endGame();
 //   }
 // }
 
 function endGame(counterTwo) {
-  this.clearInterval(counterTwo);//clear all intervals
-  // this.document.getElementById('board').onclick = null; 
-  $('#board').on = null;
-  alert("GAME OVER!" + "\nYou made " + score + " pots of GUACAMOLE! ¡Andale!");
+  $(".scoreBoard").html("GAME OVER!" + "\nYou made " + score + " pots of GUACAMOLE! ¡Andale!");
+  timerInterval   = clearInterval(timerInterval);
+  squaresInterval = clearInterval(squaresInterval);
+
+  console.log(timerInterval, squaresInterval)
 }
-
-
